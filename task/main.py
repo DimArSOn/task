a2b65cd1435e4c633ca5c1441c7dc93d2736f8aa
Dima <38756313+DimArSOn@users.noreply.github.com>
2018-05-05 22:40:57 +0300

    Add files via upload
    Additional information about me

---
+++ task/main.py
@@ -0,0 +1,6 @@
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [b"Hi, my name is Dima, i'm 19.One of my main ability is assiduity :) I'm almost graduate of Kiev college of communication. 
+  I have a great enthusiasm in general but i try to find out the direction in this big world which called IT in order to get new skills and experience. 
+Also i like to go to the gym, hang uot with friends, meet new people, teaching something new. I like to understend that i'm in constantly self-development. 
+I'm follower of healty lifestyle."]
